15c2be4206a38c3347c08d361a8cdc81ddae82bf
Artem <demnart93@gmail.com>
2020-12-06 20:39:49 +0200

    Added a second scenario of successful login.

--- login_page_test.js
+++ login_page_test.js
@@ -9,15 +9,24 @@ Before(({ I }) => {
 
 });
 
-Scenario('SuccessfulLogin', ({ I }) => {
+Scenario('SuccessfulLoginWithFirstUser', ({ I }) => {
     I.fillField('log' , 'testuser');
     I.fillField('pwd', 'testuser');
     I.checkOption('rememberme');
     I.click('wp-submit');
-    I.saveScreenshot('success.png');
+    I.saveScreenshot('FirstUserSuccess.png');
     I.waitForClickable('#site-footer > div > a');
 
 });
 
+
+Scenario('SuccesfulLoginWithSecondUser', ({ I }) => {
+    I.fillField('//*[@id="user_login"]','testuser1');
+    I.fillField('//*[@id="user_pass"]','Mae9iGei6m');
+    I.checkOption('//*[@id="rememberme"]');
+    I.click('//*[@id="wp-submit"]');
+    I.saveScreenshot('secondUserSuccess.png');
+    I.waitForClickable('#site-footer > div > a');
+});
 
 
