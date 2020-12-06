759586d9d886046e03329d175edee67311c3f4e7
Artem <demnart93@gmail.com>
2020-12-06 21:25:21 +0200

    Checking the login form when using an existing user and incorrect password.

--- login_page_test.js
+++ login_page_test.js
@@ -6,32 +6,42 @@ Before(({ I }) => {
 
 });
 
-Scenario('Checking the login form when using an existing user', ({ I }) => {
-    I.fillField('log' , 'testuser');
-    I.fillField('pwd', 'testuser');
-    I.checkOption('rememberme');
-    I.click('wp-submit');
-    I.saveScreenshot('FirstUserSuccess.png');
-    I.waitForClickable('#site-footer > div > a');
+// Scenario('Checking the login form when using an existing user', ({ I }) => {
+//     I.fillField('log' , 'testuser');
+//     I.fillField('pwd', 'testuser');
+//     I.checkOption('rememberme');
+//     I.click('wp-submit');
+//     I.saveScreenshot('FirstUserSuccess.png');
+//     I.waitForClickable('#site-footer > div > a');
 
-});
+// });
 
 
-Scenario('Checking the login form when using the second existing user', ({ I }) => {
+// Scenario('Checking the login form when using the second existing user', ({ I }) => {
+//     I.fillField('//*[@id="user_login"]','testuser1');
+//     I.fillField('//*[@id="user_pass"]','Mae9iGei6m');
+//     I.checkOption('//*[@id="rememberme"]');
+//     I.click('//*[@id="wp-submit"]');
+//     I.saveScreenshot('secondUserSuccess.png');
+//     I.waitForClickable('#site-footer > div > a');
+// });
+
+
+// Scenario('Checking the login form when a non-existent user is used', ({ I }) => {
+//     I.fillField('//*[@id="user_login"]','elvin');
+//     I.fillField('//*[@id="user_pass"]','13247');
+//     I.checkOption('//*[@id="rememberme"]');
+//     I.click('//*[@id="wp-submit"]');
+//     I.waitForText('Unknown username.');
+//     I.saveScreenshot('LoginWithNonExistentUser.png');
+// });
+
+Scenario('Checking the login form when using an existing user and incorrect password.', ({ I }) => {
     I.fillField('//*[@id="user_login"]','testuser1');
-    I.fillField('//*[@id="user_pass"]','Mae9iGei6m');
+    I.fillField('//*[@id="user_pass"]','Teechaep8a');
     I.checkOption('//*[@id="rememberme"]');
     I.click('//*[@id="wp-submit"]');
-    I.saveScreenshot('secondUserSuccess.png');
-    I.waitForClickable('#site-footer > div > a');
-});
+    I.waitForText('Lost your password?');
+    I.saveScreenshot('LoginWithExistentUserAndIncorrectPassword.png');
 
-
-Scenario('Checking the login form when a non-existent user is used', ({ I }) => {
-    I.fillField('//*[@id="user_login"]','elvin');
-    I.fillField('//*[@id="user_pass"]','13247');
-    I.checkOption('//*[@id="rememberme"]');
-    I.click('//*[@id="wp-submit"]');
-    I.waitForText('Unknown username.');
-    I.saveScreenshot('LoginWithNonExistentUser.png');
 });
