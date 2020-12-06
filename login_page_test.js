eba78712acce52c9dc1979214ad4af65f9d65676
Artem <demnart93@gmail.com>
2020-12-06 21:04:25 +0200

    Change the name of the Scenarios to make them more understandable.

--- login_page_test.js
+++ login_page_test.js
@@ -6,7 +6,7 @@ Before(({ I }) => {
 
 });
 
-Scenario('SuccessLoginWithCreatedUser', ({ I }) => {
+Scenario('Checking the login form when using an existing user', ({ I }) => {
     I.fillField('log' , 'testuser');
     I.fillField('pwd', 'testuser');
     I.checkOption('rememberme');
@@ -17,7 +17,7 @@ Scenario('SuccessLoginWithCreatedUser', ({ I }) => {
 });
 
 
-Scenario('SuccessLoginWithSecondCreatedUser', ({ I }) => {
+Scenario('Checking the login form when using the second existing user', ({ I }) => {
     I.fillField('//*[@id="user_login"]','testuser1');
     I.fillField('//*[@id="user_pass"]','Mae9iGei6m');
     I.checkOption('//*[@id="rememberme"]');
