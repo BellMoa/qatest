045daa5516e9bd7799fb3ffdc0ff27f41df6388d
Artem <demnart93@gmail.com>
2020-12-06 19:19:52 +0200

    Add Before Section

--- login_page_test.js
+++ login_page_test.js
@@ -1,5 +1,14 @@
+const { before } = require("lodash");
+
 Feature('loginPageTest');
 
+Before(({ I }) => {
+
+    I.amOnPage('login-page');
+
+});
+
 Scenario('test something', ({ I }) => {
+    
 
 });
